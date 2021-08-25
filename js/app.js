d890fb963eada2f1c48d20296b913e7f35e192f4
Slobodan Moraca <moracaslobodan@gmail.com>
2021-08-25 15:15:44 +0200

    finish

--- js/app.js
+++ js/app.js
@@ -5,6 +5,15 @@ textArea.focus()
 
 textArea.addEventListener('keyup', (e) =>{
     createTags(e.target.value)
+
+    if(e.key === 'Enter'){
+        
+        setTimeout(() =>{
+            e.target.value = ''
+        }, 10)
+
+        randomSelect()
+    }
 })
 
 function createTags(input){
@@ -15,7 +24,44 @@ function createTags(input){
     tags.forEach(tag => {
         const tagEl = document.createElement('span')
         tagEl.classList.add('tag')
-        tagEl.innerHTML = tag
+        tagEl.innerText = tag
         tagsEl.appendChild(tagEl)
     });
+}
+
+function randomSelect(){
+    const times = 30
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+
+        highLightTag(randomTag)
+
+        setTimeout(() => {
+            unHighLightTag(randomTag)
+        }, 100)
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highLightTag (randomTag)
+        }, 100);
+
+    }, times * 100);
+}
+
+function pickRandomTag(){
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highLightTag(tag){
+    tag.classList.add('highlight')
+}
+function unHighLightTag(tag){
+    tag.classList.remove('highlight')
 }
