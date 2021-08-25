077d600c98f5f61bca96bfd03f2d11f248e47673
Slobodan Moraca <moracaslobodan@gmail.com>
2021-08-25 14:26:00 +0200

    adding js

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const tagsEl = document.querySelector('#tags')
+const textArea = document.querySelector('#textarea')
+
+textArea.focus()
+
+textArea.addEventListener('keyup', (e) =>{
+    createTags(e.target.value)
+})
+
+function createTags(input){
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+    
+    tagsEl.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerHTML = tag
+        tagsEl.appendChild(tagEl)
+    });
+}
